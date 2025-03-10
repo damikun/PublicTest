e50711fbe53ea6dff820cd167a17f0ffa27d1f8e
Anka <runner@Mac-1741589885186.local>
2025-03-10 07:11:02 +0000

    Release SDK version 1.1.21 and update Package.swift checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "MyLibrary",
             url: "https://github.com/damikun/PublicTest/releases/download/v1.0.0/MyLibrary.zip", 
-            checksum: "0ef6cf362148763dd20e3eb455017e419967984247735f8e9cf611a244ea8b85"  // Update this with the actual checksum
+            checksum: "f36a55afcef60acbde53ee0a1e7a085ac3dbcfbfbbe15491d0a1f20a4f4a746b"  // Update this with the actual checksum
         ),
     ]
 )
